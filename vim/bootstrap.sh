2d41d3507251bf1b2c4794c27f664a0fc035dbf7
Why8n <Why8n@gmail.com>
2020-05-08 21:20:04 +0800

    update structure

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
-ln -rs .vim ~/.vim
-# for vim
-ln -rs .vimrc ~/.vimrc
-# for neovim
-FILE=~/.config/nvim
-if [ ! -d "$FILE" ]; then     
-	echo "create directory $FILE"
-	mkdir -p "$FILE"
+[ -f ~/.vimrc ] && rm -v ~/.vimrc
+[ -d ~/.vim ] && rm -rfv ~/.vim
+[ -f .config/nvim/init.vim ] && rm -v .config/nvim/init.vim
+FILE=~/.config
+if [ -d "$FILE" ]; then     
+	rm -rfv "$FILE"
 fi
-ln -rs .vimrc ~/.config/nvim/init.vim
+
+ln -rs .vim ~/.vim && echo link .vim done
+# for vim
+ln -rs ../.vimrc ~/.vimrc && echo link .vimrc done
+# for neovim
+ln -rs .config ~/.config && echo link .config done
+ln -rs ../.vimrc ~/.config/nvim/init.vim && echo link .init.vim done
 
 
 
