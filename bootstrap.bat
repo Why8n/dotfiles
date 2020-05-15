dacf4b7e552698b3d41eebe84ce22f84cd31b6d1
Why8n <whyncai@gmail.com>
2020-05-15 21:01:33 +0800

    add installation setup for windows terminal

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,10 +1,21 @@
 @echo off
-del %HOME%\.npmrc
-mklink %HOME%\.npmrc %CD%\.npmrc
 
+set cur_path=%CD%
+
+REM  npm configuration
+del %HOME%\.npmrc
+mklink %HOME%\.npmrc %cur_path%\.npmrc
+
+REM  pip configuration
 rmdir /s /q %HOME%\pip
 mkdir %HOME%\pip
-mklink %HOME%\pip\pip.ini %CD%\pip.ini
+mklink %HOME%\pip\pip.ini %cur_path%\pip.ini
 
-cd vim
+REM  vim configuration
+cd %cur_path%\vim
 start bootstrap.bat
+
+REM  windows terminal configuration
+cd %cur_path%\windows\terminal
+start bootstrap.bat
+
--- windows/terminal/bootstrap.bat
+++ windows/terminal/bootstrap.bat
@@ -1,2 +1,4 @@
 @echo off
+del %AppData%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+
 mklink %AppData%\..\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %cd%\settings.json
