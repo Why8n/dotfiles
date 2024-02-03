f3da04578c1318e49900201a91eb8226b6f9f187
Why8n <Why8n@gmail.com>
2024-02-03 20:54:03 +0800

    feat(neovim): install & configure plugin - folke/flash.nvim

--- vim/.config/nvim/lua/plugins/utility.lua
+++ vim/.config/nvim/lua/plugins/utility.lua
@@ -1,7 +1,27 @@
 return {
-  'windwp/nvim-autopairs',
-  event = "InsertEnter",
-  opts = { -- this is equalent to setup({}) function
-    enable_check_bracket_line = true,
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = { -- this is equalent to setup({}) function
+      enable_check_bracket_line = true,
+    }
+  },
+
+  {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          -- disable f, F, t, T 
+          enabled = false
+        }
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader><leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    },
   }
 }
