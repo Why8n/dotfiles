ac67697c330965cdb29eb0d84f16f5995d8843d3
Why8n <Why8n@gmail.com>
2024-02-08 13:10:08 +0800

    feat(neovim): update configurations for vscode-neovim

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,7 +1,10 @@
-require("basic.options")
-require("basic.keymaps")
-
--- load plugin manager first
-require("setup")
-
+require('basic.options')
+require('basic.keymaps')
+if vim.g.vscode then
+    require('vscode.options')
+    require('vscode.keymaps')
+else
+    -- load plugin manager first
+    require('setup')
+end
 
---
+++ vim/.config/nvim/lua/vscode/options.lua
@@ -0,0 +1,3 @@
+-- set vscode.notify as neovim's default notify function
+vim.notify = require('vscode-neovim').notify
+
